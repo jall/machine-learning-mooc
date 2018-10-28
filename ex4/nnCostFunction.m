d2300030a33774fd012e30583e56e821b40be68e
Jon Hall <jon.hall@candide.eu>
2018-10-28 17:36:48 +0000

    4: Feed forward & unregularised cost

--- ex4/nnCostFunction.m
+++ ex4/nnCostFunction.m
@@ -8,8 +8,8 @@ function [J grad] = nnCostFunction(nn_params, ...
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
 %   X, y, lambda) computes the cost and gradient of the neural network. The
 %   parameters for the neural network are "unrolled" into the vector
-%   nn_params and need to be converted back into the weight matrices. 
-% 
+%   nn_params and need to be converted back into the weight matrices.
+%
 %   The returned parameter grad should be a "unrolled" vector of the
 %   partial derivatives of the neural network.
 %
@@ -24,8 +24,9 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 
 % Setup some useful variables
 m = size(X, 1);
-         
-% You need to return the following variables correctly 
+y = convertToUnitColumnVectors(y', num_labels);
+
+% You need to return the following variables correctly
 J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
@@ -38,6 +39,20 @@ Theta2_grad = zeros(size(Theta2));
 %         variable J. After implementing Part 1, you can verify that your
 %         cost function computation is correct by verifying the cost
 %         computed in ex4.m
+
+H = feedForward(X, Theta1, Theta2, num_labels);
+
+unregularisedCost = 0;
+for i = 1:m
+  y_i = y(:, i);
+  h_i = H(:, i);
+  costOfExample = sum((-y_i' * log(h_i)) - ((1 - y_i)' * log(1 - h_i)));
+  unregularisedCost += costOfExample;
+endfor
+unregularisedCost *= (1 / m);
+
+J = unregularisedCost;
+
 %
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
@@ -46,12 +61,12 @@ Theta2_grad = zeros(size(Theta2));
 %         that your implementation is correct by running checkNNGradients
 %
 %         Note: The vector y passed into the function is a vector of labels
-%               containing values from 1..K. You need to map this vector into a 
+%               containing values from 1..K. You need to map this vector into a
 %               binary vector of 1's and 0's to be used with the neural network
 %               cost function.
 %
 %         Hint: We recommend implementing backpropagation using a for-loop
-%               over the training examples if you are implementing it for the 
+%               over the training examples if you are implementing it for the
 %               first time.
 %
 % Part 3: Implement regularization with the cost function and gradients.
@@ -62,24 +77,6 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 % -------------------------------------------------------------
 
 % =========================================================================
@@ -87,5 +84,35 @@ Theta2_grad = zeros(size(Theta2));
 % Unroll gradients
 grad = [Theta1_grad(:) ; Theta2_grad(:)];
 
+
+end
+
+
+function H = feedForward(X, Theta1, Theta2, num_labels)
+
+m = size(X, 1);
+
+A_1 = [ones(1, m); X'];
+A_2 = [ones(1, m); sigmoid(Theta1 * A_1)];
+A_3 = sigmoid(Theta2 * A_2);
+
+H = A_3;
+
+end
+
+
+function A = convertToUnitColumnVectors(X, column_vector_length)
+
+A_but_1D = cell2mat(arrayfun(@(x) toUnitVector(x, column_vector_length), X, "UniformOutput", false));
+[width, height] = size(A_but_1D);
+A = reshape(A_but_1D(:)(:), width, height);
+
+end
+
+
+function v = toUnitColumnVector(idx, len)
+
+v = zeros(len, 1);
+v(idx) = 1;
 
 end
