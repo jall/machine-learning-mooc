94acba9538022e65971efb3dc8921ba4bf55bd89
Jon Hall <jon.hall@candide.eu>
2018-12-26 18:06:02 +0000

    8: Select episilon threshold

--- ex8/selectThreshold.m
+++ ex8/selectThreshold.m
@@ -12,30 +12,30 @@ F1 = 0;
 
 stepsize = (max(pval) - min(pval)) / 1000;
 for epsilon = min(pval):stepsize:max(pval)
-    
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Compute the F1 score of choosing epsilon as the
-    %               threshold and place the value in F1. The code at the
-    %               end of the loop will compare the F1 score for this
-    %               choice of epsilon and set it to be the best epsilon if
-    %               it is better than the current choice of epsilon.
-    %               
-    % Note: You can use predictions = (pval < epsilon) to get a binary vector
-    %       of 0's and 1's of the outlier predictions
+    predictions = (pval < epsilon);
 
+    truePositives = sum((predictions == 1) & (yval == 1));
+    falsePositives = sum((predictions == 1) & (yval == 0));
+    trueNegatives = sum((predictions == 0) & (yval == 0));
+    falseNegatives = sum((predictions == 0) & (yval == 1));
 
+    if (truePositives + falsePositives) ~= 0
+      precision = truePositives / (truePositives + falsePositives);
+    else
+      precision = 0;
+    endif
 
+    if (truePositives + falseNegatives) ~= 0
+      recall = truePositives / (truePositives + falseNegatives);
+    else
+      recall = 0;
+    endif
 
-
-
-
-
-
-
-
-
-
-    % =============================================================
+    if (precision + recall) ~= 0
+      F1 = (2 * precision * recall) / (precision + recall);
+    else
+      F1 = 0;
+    endif
 
     if F1 > bestF1
        bestF1 = F1;
